667b427658c160ccc971641883464e67bcfef5a4
aymeric af. figerou <figerou@dev.afpa>
2017-03-23 13:49:26 +0100

    Polify

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,26 @@
 var codesPostaux = require('codes-postaux');
 
+var messageConseilReponse = "Entrez un code postal sous format 75000, prenez en compte l'arrondissement";
 
 exports.get = function(req, res){
-		res.render('index',{test:"Details"}, function(err, html){
+		res.render('index',{messageConseil: messageConseilReponse, retourCP:""}, function(err, html){
 			res.send(html);
 	});
 }
 
 
 exports.post = function(req, res){
-		var pc = codesPostaux.find(req.body.pc)
-		res.render('index',{test: pc[0].nomCommune}, function(err, html){
-			res.send(html);
-	});
+		var cp = codesPostaux.find(req.body.cp)
+		if(!cp[0]){
+			res.render('index',{messageConseil: messageConseilReponse, retourCP: "Vous n'avez pas correctement tapper le code postal"}, function(err, html){
+				res.send(html);
+			});
+			
+		}else{
+			res.render('index',{messageConseil: messageConseilReponse,retourCP: cp[0].nomCommune}, function(err, html){
+				res.send(html);
+			});
+		}
+		
+		
 }
